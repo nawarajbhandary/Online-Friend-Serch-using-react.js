965dd2867a53edf7f0d1d64c0fa5baa36a25d8c2
nawarajbhandary <nawarajbhandary03@gmail.com>
2021-09-21 19:17:42 +0545

    Refactor successly, A Online Friend Search App has been refactor using HOOKS by replacing the class componet #Now state things become easier with HOOKS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FriendItem from "./Frienditem";
 
 const friendList = [
@@ -13,7 +13,7 @@ const friendList = [
     title : "Creative Designer"
 },
 {
-    imageUrl : "https://scontent.fktm8-1.fna.fbcdn.net/v/t39.30808-6/233367324_2821234264782458_3854471340021096011_n.jpg?_nc_cat=101&_nc_rgb565=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=4riJ27oNkfoAX_SlAT0&_nc_ht=scontent.fktm8-1.fna&oh=3ee198265e77c94642728243f7bd703e&oe=612E1899",
+    imageUrl : "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
     name : "Raj Kumar Khadka",
     title : "Web Develepor"
 },
@@ -23,42 +23,49 @@ const friendList = [
     title : "ASP .net Developer"
 },
 {
-    imageUrl : "https://scontent.fktm8-1.fna.fbcdn.net/v/t1.6435-9/60254260_2307683819474681_1221678931673874432_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ccmet4N6XBMAX9qN6A7&_nc_ht=scontent.fktm8-1.fna&oh=be3dc9e0d22d35c0fec6711f72eb12e6&oe=614DD971",
+    imageUrl : "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
     name : "Bishu Thapa",
     title : "Trader & Investor"
 },
 ];
 
-class App extends Component{
-    constructor(props) {
-        super(props);
+const App = () => {
+    const [searchText, setSearchText] = useState(""); // No constructor, No state, No setState just before state and SetState !! BOOMMM !!!
 
-        this.state = {
-            searchText : "" ,   //Comma here but not SEMICOLON
-        };
-    }
-handleChange = (e) => {
+   /* 
+   Way of cloning the array in javascript
+
+    We must should make clone of the value before applying any change on value, and it is mandatory for the primitive dataTypes like object and Array
+
+   const [todos, setTodos] = useState([]); -> This is way of initializing the state inCase of Array
+    
+    const handleTodoAdd = () => {
+        const todosCloned = todos.slice();  //Slice is the way of making clone of the array
+        const todosCloned = [...todos];  //Using spread operator ALSO we can make the clone of the array
+        todosCloned.push("Finish the class"); 
+    } 
+    
+    */
+
+    const handleChange = (e) => {
     const value = e.target.value;
-    this.setState({
-        searchText :value,
-    });
+    setSearchText(value); //The hassle on using the setState and assighing value will replaced using the setSearchText
 };
 
-filterFriend = () => {
+const filterFriend = () => {
     return friendList.filter(item => {
-        return item.name.toLowerCase().includes(this.state.searchText.toLowerCase());
+        return item.name.toLowerCase().includes(searchText.toLowerCase());
     });
 }
 
-    render(){
-        const filtered = this.filterFriend();
+        const filtered =filterFriend(); //previously this.filtered friend in class Component but not needed in Hooks
         return(
             <div>
                 <h2>Online Friend Search</h2>
                
                 <input placeholder = "Search" 
-                onChange={this.handleChange} 
-                value = {this.state.searchText}
+                onChange={handleChange} 
+                value = {searchText}
                  />
 
 
@@ -92,7 +99,6 @@ filterFriend = () => {
                  })} 
             </div>
         );
-    }
 }
 
 export default App;
